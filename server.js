6934197eb564c43b3fde20b1e6cc251629e29407
IEK32TR <capserman@gmail.com>
2024-11-15 20:47:37 +0300

    username
    sadece kaldığım yeri kaydediyorum hala eksik

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const path = require('path');
-const http = require('http'); // Required for creating server
-const { Server } = require('socket.io'); // Import Server from socket.io
+const http = require('http');
+const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Store leaderboard data in memory (could be replaced with a database)
+let leaderboard = [];
+
 // Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname)));
 
-// Example Socket.IO setup (only if you need real-time communication)
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
+// Parse JSON request bodies
+app.use(express.json());
 
 // Serve index.html when accessing the root URL
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'game.html'));
+});
+
+// Serve leaderboard page
+app.get('/leaderboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'leaderboard.html'));
+});
+
+// Save score when a user finishes the game
+app.post('/save-score', (req, res) => {
+    const { username, score } = req.body;
+    if (score >= 8) {
+        leaderboard.push({ username, score });
+        leaderboard.sort((a, b) => b.score - a.score); // Sort leaderboard by score (highest first)
+    }
+    res.status(200).send();
+});
+
+// Fetch leaderboard data
+app.get('/leaderboard-data', (req, res) => {
+    res.json(leaderboard);
 });
 
 server.listen(8082, () => {

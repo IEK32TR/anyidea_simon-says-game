57da0a97dfa46155020f0238649adf9c97247db8
thefumiu <Uysalfurkanmirac@icloud.com>
2024-11-16 13:14:47 +0300

    Leaderboard

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+let minscore = 7;
 let leaderboard = [];  // Store leaderboard data in memory
 
 app.use(express.static(path.join(__dirname)));
@@ -20,21 +21,35 @@ app.get('/leaderboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'leaderboard.html'));
 });
 
+app.get('/leaderboard-t', (req, res) => {
+    leaderboard.push({username: "Furkan", score: 16 });
+    leaderboard.push({username: "İsmet", score: 8 });
+    leaderboard.push({username: "Enes", score: 8 });
+    res.redirect("/leaderboard");
+});
+
+app.get('/leaderboard-r', (req, res) => {
+    leaderboard = [];
+    res.redirect("/leaderboard");
+});
+
+app.get('/leaderboard-min/:id', (req, res) => {
+    minscore = req.params.id;
+    res.redirect("/leaderboard");
+});
+
 // Save score API
 app.post('/save-score', (req, res) => {
     const { username, score } = req.body;
-    if (score >= 7) { // Modify this rule if needed
-        leaderboard.push({ username, score });
-        leaderboard.sort((a, b) => b.score - a.score); // Sort leaderboard
-    }
+    leaderboard.push({ username, score });
     res.status(200).send();
 });
 
 // API to fetch leaderboard data
 app.get('/leaderboard-data', (req, res) => {
-    res.json(leaderboard);
+    res.json(leaderboard.filter(item => item.score > minscore).sort((a, b) => b.score - a.score));
 });
 
-server.listen(8082, () => {
-    console.log('Server running at http://localhost:8082');
+server.listen(8083, () => {
+    console.log('Server running at http://localhost:8083');
 });
